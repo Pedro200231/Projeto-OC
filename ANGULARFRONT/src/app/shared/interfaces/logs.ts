a4f33ef4af2dd0ba767044a1e995cfd6a6c2fbdb
João Pedro Odorizzio de Souza <joao.310502@alunos.utfpr.edu.br>
2024-02-26 08:30:20 -0300

    Delete

--- ANGULARFRONT/src/app/shared/interfaces/logs.ts
+++
@@ -1,8 +0,0 @@
-export interface Logs {
-  id: number;
-  action: string;
-  targetEmail: string;
-  timestamp: Date;
-  beforeData: string;
-  afterData: string;
-}
--- ANGULARFRONT/src/environments/environment.development.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-  API: 'https://localhost:7178/'
-};
